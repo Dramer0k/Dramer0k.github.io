21fda106102612a94147afeb41fc4aa45362e20c
Dramer0k <icecreamkazan@gmail.com>
2022-07-30 16:58:58 +0300

    add scss
    go

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,13 @@
 /* Test Git */
+let registration = document.getElementById('login');
+
+registration.classList= function () {
+    login.color = 'red'
+}
+let call = document.getElementById('connection')
+let callBlock = document.getElementById('connection-block')
+
+call.onclick = function() {
+    console.log('Open!')
+    callBlock.classList.toggle('open')
+}
